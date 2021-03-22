ff7badb6a69a4a59749c39f6685055c2879eb785
Alexandr-Prohorov <alexandr@MacBook-Pro-Aleksandr.local>
2021-03-22 13:00:38 +0300

    Добавил массив с объектами

--- js/main.js
+++ js/main.js
@@ -24,6 +24,21 @@ let userComments = [ 'Всё отлично!',
 
 let userName = ['Артем', 'Максим', 'Александр', 'Павел', 'Юлия', 'Игорь', 'Вероника', 'Полина', 'Евгения']
 
+let generationComment = function (){
+  let comment = []
+  let numberComment = randomNumber(1,4)
+  for (let i = 0; i <= numberComment; i++){
+    let comments = {
+      id: i + 1,
+      avatar: ('img/avatar-' + randomNumber (1, 6) + '.svg'),
+      message: userComments[randomNumber(1,6)-1],
+      user: userName[randomNumber(1,9)-1],
+    }
+    comment.push(comments)
+  }
+  return comment
+}
+
 let generationObjects = function (){
   for (let i=0; i <= 24; i++){
     let obj = {
@@ -31,17 +46,16 @@ let generationObjects = function (){
       url: ('photos/' + (i + 1) + '.jpg'),
       description: ('Описание'),
       likes: randomNumber (15, 200),
-      comments: {
-        id: i + 1,
-        avatar: ('img/avatar-' + randomNumber (1, 6) + '.svg'),
-        message: userComments[randomNumber(1,6)-1],
-        user: userName[randomNumber(1,9)-1],
-      },
+      comments: generationComment(),
     }
     objects.push(obj)
   }
 }
 generationObjects();
+
+
+
+
 
 for (let i = 0; i <= objects.lenght - 2; i++) {
   let value = objects[i].id;
@@ -52,3 +66,4 @@ for (let i = 0; i <= objects.lenght - 2; i++) {
   }
 }
 
+console.log(objects)
